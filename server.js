7579f99d6f89f0b20bf6d209ca481cbfcece67c4
Boyuan Shi <sbyshjd@hotmail.com>
2020-10-09 18:38:26 +0200

    server run dev

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const cors   = require('cors');
 
+
+server.use(cors({
+  credentials:true,
+  origin:['http://localhost:3000']
+}));
 server.use(router)
 server.listen(8000, () => {
   console.log('JSON Server is running')
